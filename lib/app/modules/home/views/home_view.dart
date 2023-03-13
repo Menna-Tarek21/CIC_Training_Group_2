1f11dbeacf86f5742d5027b26115831128448028
Menna-Tarek21 <120463043+Menna-Tarek21@users.noreply.github.com>
2023-03-13 15:14:27 +0200

    work

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,15 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
-Widget? model(url){ return Padding(
+Widget? modelImages(url){ return Padding(
     padding: const EdgeInsets.all(8.0),
   child: Container(height: 100,child: Image.asset(url), decoration: BoxDecoration(borderRadius: BorderRadius.circular(10))),);
 }
+Widget? modelIcon(url){
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration( color: Colors.white,borderRadius: BorderRadius.circular(10)),
+      child: Image.asset(url),
+    ),
+  );
 
-dynamic list = [
-  model("asset/WhatsApp-Image-2023-02-15-at-1.48.45-PM-1024x768.jpeg"),
-  model("asset/885682201615729680.webp"), 
-  model("asset/Misr_El-Kheir.jpg"),
+}
+dynamic listIcon =  [  modelIcon("asset/sun.png"),modelIcon("asset/sun.png"),modelIcon("asset/sun.png"),modelIcon("asset/sun.png"),modelIcon("asset/sun.png"),modelIcon("asset/sun.png"),modelIcon("asset/sun.png")];
+dynamic listImage = [
+  modelImages("asset/WhatsApp-Image-2023-02-15-at-1.48.45-PM-1024x768.jpeg"),
+  modelImages("asset/885682201615729680.webp"),
+  modelImages("asset/Misr_El-Kheir.jpg"),
 ];
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
@@ -61,26 +73,13 @@ crossAxisAlignment: CrossAxisAlignment.start,
 
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: List.generate(10, (index) {
-                      return   Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration( color: Colors.white,borderRadius: BorderRadius.circular(10)),
-                          child: Image.asset("asset/sun.png"),
-                        ),
-                      );
-
-                    }),
-                  ),
-                )
+                    children: List.from(listIcon)),)
                 ,
                 SizedBox(height: 40,),
                 SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Row(
-                    children: List.from(list),
+                    children: List.from(listImage),
                   ),
                 )
               ],
