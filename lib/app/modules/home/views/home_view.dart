1995ce7e8f655e76a4e0bd0936bff1516ac326ca
MohamedRahman <atpfreelancer@gmail.com>
2023-03-13 13:52:40 +0200

    homepage

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,17 +8,41 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.grey[400],
+          body: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Welcome",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                      ),
+                    ),
+                    Text(
+                      "To Misr Al Khair",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Container(
+                  child: Image.asset("asset/Iconly-Bulk-Notification.png"),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
